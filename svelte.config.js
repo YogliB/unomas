8d23a04fb14e662800ddd044aec6b9efc24b7f10
YogliB <yogiboaron@gmail.com>
2021-05-15 11:08:25 +0300

    set environment

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
 import preprocess from 'svelte-preprocess';
+import windicss from 'svelte-windicss-preprocess';
+const { windi } = windicss;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			defaults: {
+				script: 'typescript',
+				style: 'scss',
+			},
+		}),
+		windi({}),
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		target: '#svelte',
+	},
 };
 
 export default config;
